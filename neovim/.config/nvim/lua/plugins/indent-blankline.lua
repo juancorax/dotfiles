6c60ac85ef297028ff37f04c3027d2f7dd32a17c
Juan Manuel Corigliano Giuliani <126042007+juancorax@users.noreply.github.com>
2024-02-13 03:49:53 -0300

    add completion & indent guides plugins

---
+++ neovim/.config/nvim/lua/plugins/indent-blankline.lua
@@ -0,0 +1,10 @@
+return {
+  "lukas-reineke/indent-blankline.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  main = "ibl",
+  config = function()
+    require("ibl").setup({
+      scope = { enabled = false },
+    })
+  end,
+}
