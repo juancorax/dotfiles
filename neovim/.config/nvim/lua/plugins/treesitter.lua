8c24261c4c5e2c580317b851d400cac10a49f2b0
Juan Manuel Corigliano Giuliani <126042007+juancorax@users.noreply.github.com>
2024-02-13 18:14:50 -0300

    add treesitter language parsers

--- neovim/.config/nvim/lua/plugins/treesitter.lua
+++ neovim/.config/nvim/lua/plugins/treesitter.lua
@@ -4,6 +4,7 @@ return {
   build = ":TSUpdate",
   config = function()
     require("nvim-treesitter.configs").setup({
+      ensure_installed = { "c", "lua", "vim", "vimdoc", "query" },
       auto_install = true,
       highlight = { enable = true },
       indent = { enable = true },
