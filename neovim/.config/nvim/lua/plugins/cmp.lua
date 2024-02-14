e85356c8c8bacfdd8cfcd30bdba14bcdd8645f65
Juan Manuel Corigliano Giuliani <126042007+juancorax@users.noreply.github.com>
2024-02-13 23:41:56 -0300

    add rails snippets

--- neovim/.config/nvim/lua/plugins/cmp.lua
+++ neovim/.config/nvim/lua/plugins/cmp.lua
@@ -12,6 +12,8 @@ return {
     local cmp = require("cmp")
     local luasnip = require("luasnip")
 
+    luasnip.filetype_extend("ruby", {"rails"})
+
     require("luasnip.loaders.from_vscode").lazy_load()
 
     cmp.setup({
