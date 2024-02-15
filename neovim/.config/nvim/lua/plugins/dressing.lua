6bd60487b5b21b7367e1920fa25c1cdbac4c8c69
Juan Manuel Corigliano Giuliani <126042007+juancorax@users.noreply.github.com>
2024-02-15 17:44:23 -0300

    make dressing.nvim corners square

--- neovim/.config/nvim/lua/plugins/dressing.lua
+++ neovim/.config/nvim/lua/plugins/dressing.lua
@@ -1,4 +1,20 @@
 return {
   "stevearc/dressing.nvim",
   event = "VeryLazy",
+  config = function()
+    require("dressing").setup({
+      input = {
+        border = "single",
+      },
+      select = {
+        telescope = require("telescope.themes").get_dropdown({
+          borderchars = {
+            prompt = { "─", "│", " ", "│", "┌", "┐", "│", "│" },
+            results = { "─", "│", "─", "│", "├", "┤", "┘", "└" },
+            preview = { "─", "│", "─", "│", "┌", "┐", "┘", "└" },
+          },
+        }),
+      },
+    })
+  end,
 }
