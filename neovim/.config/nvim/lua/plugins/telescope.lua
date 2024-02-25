b31f719313576b6adca2e9e5eba376301c02e356
Juan Manuel Corigliano Giuliani <126042007+juancorax@users.noreply.github.com>
2024-02-25 12:15:51 -0300

    fix telescope path_display

--- neovim/.config/nvim/lua/plugins/telescope.lua
+++ neovim/.config/nvim/lua/plugins/telescope.lua
@@ -4,7 +4,7 @@ return {
   branch = "0.1.x",
   dependencies = {
     "nvim-lua/plenary.nvim",
-    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" }
+    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
   },
   config = function()
     local telescope = require("telescope")
@@ -12,24 +12,24 @@ return {
 
     telescope.setup({
       defaults = {
-        path_display = "truncate",
+        path_display = { "truncate" },
         borderchars = { "─", "│", "─", "│", "┌", "┐", "┘", "└" },
         mappings = {
           i = {
             ["<C-k>"] = actions.move_selection_previous,
-            ["<C-j>"] = actions.move_selection_next
-          }
-        }
-      }
+            ["<C-j>"] = actions.move_selection_next,
+          },
+        },
+      },
     })
     telescope.load_extension("fzf")
 
     local keymap = vim.keymap.set
     local builtin = require("telescope.builtin")
     local function live_grep_open_files()
-      builtin.live_grep {
-        grep_open_files = true
-      }
+      builtin.live_grep({
+        grep_open_files = true,
+      })
     end
     keymap("n", "<leader>ff", builtin.find_files, { desc = "lists files in your current working directory" })
     keymap("n", "<leader>fg", builtin.live_grep, { desc = "search for a string in your current working directory" })
