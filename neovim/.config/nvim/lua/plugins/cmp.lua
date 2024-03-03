9f9e7ccfac618f63f6340067e34e3dff1cd4a790
Juan Manuel Corigliano Giuliani <126042007+juancorax@users.noreply.github.com>
2024-03-03 09:02:52 -0300

    update mappings for completion item selection

--- neovim/.config/nvim/lua/plugins/cmp.lua
+++ neovim/.config/nvim/lua/plugins/cmp.lua
@@ -26,8 +26,8 @@ return {
         end,
       },
       mapping = cmp.mapping.preset.insert({
-        ["<C-k>"] = cmp.mapping.select_prev_item(),
-        ["<C-j>"] = cmp.mapping.select_next_item(),
+        ["<C-p>"] = cmp.mapping.select_prev_item(),
+        ["<C-n>"] = cmp.mapping.select_next_item(),
         ["<C-b>"] = cmp.mapping.scroll_docs(-4),
         ["<C-f>"] = cmp.mapping.scroll_docs(4),
         ["<C-Space>"] = cmp.mapping.complete(),
