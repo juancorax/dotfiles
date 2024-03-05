d6307487051bc31d1205a4067b7779b544827721
Juan Manuel Corigliano Giuliani <126042007+juancorax@users.noreply.github.com>
2024-03-05 11:00:28 -0300

    enable all css features in nvim-colorizer

--- neovim/.config/nvim/lua/plugins/colorizer.lua
+++ neovim/.config/nvim/lua/plugins/colorizer.lua
@@ -1,5 +1,11 @@
 return {
   "NvChad/nvim-colorizer.lua",
   event = { "BufReadPre", "BufNewFile" },
-  config = true,
+  config = function()
+    require("colorizer").setup({
+      user_default_options = {
+        css = true,
+      },
+    })
+  end,
 }
