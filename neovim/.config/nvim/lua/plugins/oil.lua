0ae7607893bde3f78b300b8e30a8f4ee952e35bf
Juan Manuel Corigliano Giuliani <126042007+juancorax@users.noreply.github.com>
2024-05-05 19:05:35 -0300

    fix: change oil.nvim borders to 'single'

--- neovim/.config/nvim/lua/plugins/oil.lua
+++ neovim/.config/nvim/lua/plugins/oil.lua
@@ -1,7 +1,23 @@
 return {
   "stevearc/oil.nvim",
   config = function()
-    require("oil").setup()
+    require("oil").setup({
+      keymaps_help = {
+        border = "single",
+      },
+      float = {
+        border = "single",
+      },
+      preview = {
+        border = "single",
+      },
+      progress = {
+        border = "single",
+      },
+      ssh = {
+        border = "single",
+      },
+    })
 
     vim.keymap.set("n", "-", "<Cmd>Oil<CR>", { desc = "open parent directory" })
   end,
