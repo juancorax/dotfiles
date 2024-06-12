5110ab6255a4db4848935143f46ae844053eac43
Juan Manuel Corigliano Giuliani <126042007+juancorax@users.noreply.github.com>
2024-06-12 16:07:52 -0300

    fix(colorscheme): remove gutter background

--- neovim/.config/nvim/lua/plugins/colorscheme.lua
+++ neovim/.config/nvim/lua/plugins/colorscheme.lua
@@ -3,6 +3,18 @@ return {
   lazy = false,
   priority = 1000,
   config = function()
+    require("kanagawa").setup({
+      colors = {
+        theme = {
+          all = {
+            ui = {
+              bg_gutter = "none",
+            },
+          },
+        },
+      },
+    })
+
     require("kanagawa").load("dragon")
   end,
 }
