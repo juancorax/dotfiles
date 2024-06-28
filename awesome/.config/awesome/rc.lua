45089149155dffcb5bfc87531244f6dc5739bfc9
Juan Manuel Corigliano Giuliani <126042007+juancorax@users.noreply.github.com>
2024-06-27 22:04:12 -0300

    fix: remove menu widget

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -215,7 +215,7 @@ screen.connect_signal("request::desktop_decoration", function(s)
       layout = wibox.layout.align.horizontal,
       { -- Left widgets
         layout = wibox.layout.fixed.horizontal,
-        mylauncher,
+        -- mylauncher,
         s.mytaglist,
         s.mypromptbox,
       },
