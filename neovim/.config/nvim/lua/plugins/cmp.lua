9db50b992f374008504f36e796f497eb52545e63
Juan Manuel Corigliano Giuliani <126042007+juancorax@users.noreply.github.com>
2024-10-15 22:15:24 -0300

    feat(neovim): add blade snippets

--- neovim/.config/nvim/lua/plugins/cmp.lua
+++ neovim/.config/nvim/lua/plugins/cmp.lua
@@ -12,6 +12,7 @@ return {
     local cmp = require("cmp")
     local luasnip = require("luasnip")
 
+    luasnip.filetype_extend("php", { "blade" })
     luasnip.filetype_extend("ruby", { "rails" })
 
     require("luasnip.loaders.from_vscode").lazy_load()
