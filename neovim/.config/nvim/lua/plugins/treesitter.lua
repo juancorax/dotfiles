aded71f59b0822d838f452573b487bb7f8b204fb
Juan Manuel Corigliano Giuliani <126042007+juancorax@users.noreply.github.com>
2024-10-17 05:50:09 -0300

    feat(treesitter): add parser for blade

--- neovim/.config/nvim/lua/plugins/treesitter.lua
+++ neovim/.config/nvim/lua/plugins/treesitter.lua
@@ -9,5 +9,21 @@ return {
       highlight = { enable = true },
       indent = { enable = true },
     })
+
+    local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+    parser_config.blade = {
+      install_info = {
+        url = "https://github.com/EmranMR/tree-sitter-blade",
+        files = { "src/parser.c" },
+        branch = "main",
+      },
+      filetype = "blade",
+    }
+
+    vim.filetype.add({
+      pattern = {
+        [".*%.blade%.php"] = "blade",
+      },
+    })
   end,
 }
