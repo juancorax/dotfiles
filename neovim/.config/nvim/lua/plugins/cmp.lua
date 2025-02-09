fecd18cca44d9999fc2ba0599c1ea5c23a9ce122
Juan Manuel Corigliano Giuliani <126042007+juancorax@users.noreply.github.com>
2025-02-09 15:26:18 -0300

    feat(neovim): add vim-dadbod plugins

--- neovim/.config/nvim/lua/plugins/cmp.lua
+++ neovim/.config/nvim/lua/plugins/cmp.lua
@@ -52,5 +52,12 @@ return {
         { name = "path" },
       }),
     })
+
+    cmp.setup.filetype({ "sql" }, {
+      sources = {
+        { name = "vim-dadbod-completion" },
+        { name = "buffer" },
+      },
+    })
   end,
 }
