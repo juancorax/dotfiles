888ce169d9568eee5ebdb60f867001f217e640e9
Juan Manuel Corigliano Giuliani <126042007+juancorax@users.noreply.github.com>
2025-07-29 00:42:03 -0300

    chore(nvim-cmp): prioritize nvim_lsp in sources list

--- neovim/.config/nvim/lua/plugins/cmp.lua
+++ neovim/.config/nvim/lua/plugins/cmp.lua
@@ -44,8 +44,8 @@ return {
         end, { "i", "s" }),
       }),
       sources = cmp.config.sources({
-        { name = "luasnip" },
         { name = "nvim_lsp" },
+        { name = "luasnip" },
         { name = "buffer" },
         { name = "path" },
       }),
